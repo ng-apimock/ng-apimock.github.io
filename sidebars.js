4862e5c4826c7330394eabe230556137a1a24d28
Mischa Dasberg <mischa@dasberg.nl>
2021-04-11 13:06:04 +0200

    chore: add debug information

--- sidebars.js
+++ sidebars.js
@@ -8,7 +8,8 @@ module.exports = {
                 'mocks',
                 'variables',
                 'presets',
-                'docker'
+                'docker',
+                'debug'
             ]
         },
         {
